bfa603af8ee875e2e614b3883998b6f34b713459
Xavier Basty <xbasty@gmail.com>
2012-09-28 15:14:00 +0200

    added missing semi-colon to declaration

--- src/vmod_abtest.h
+++ src/vmod_abtest.h
@@ -8,4 +8,4 @@ static void cfg_free(struct vmod_abtest *cfg);
 static void delete_rule(struct rule* r);
 static struct rule* get_rule(struct vmod_abtest *cfg, const char *key);
 static struct rule* alloc_rule(struct vmod_abtest *cfg, const char *key);
-static struct rule* get_rule_alloc(struct vmod_abtest *cfg, const char *key)
+static struct rule* get_rule_alloc(struct vmod_abtest *cfg, const char *key);

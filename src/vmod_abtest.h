1ed1f13dcc6843a3fdda59d56c016330b0fca87c
Francisco Alcaraz <fjalcaraz@gmail.com>
2023-06-21 12:31:01 +0200

    migration to varnish 6

--- src/vmod_abtest.h
+++ src/vmod_abtest.h
@@ -6,6 +6,8 @@ static void cfg_clear(struct vmod_abtest *cfg);
 static void cfg_free(struct vmod_abtest *cfg);
 
 static void delete_rule(struct rule* r);
-static struct rule* get_regex_rule(struct vmod_abtest *cfg, const char *key);
-static struct rule* get_text_rule(struct vmod_abtest *cfg, const char *key);
-static struct rule* alloc_rule(struct sess *sp, struct vmod_abtest *cfg, const char *key);
+static struct rule* get_regex_rule(struct vmod_abtest *cfg, VCL_STRING key);
+static struct rule* get_text_rule(struct vmod_abtest *cfg, VCL_STRING key);
+static struct rule* alloc_rule(VRT_CTX, struct vmod_abtest *cfg, VCL_STRING key);
+
+static void write_log(struct vsl_log *vsl, enum VSL_tag_e tag,  const char *fmt, ...);
